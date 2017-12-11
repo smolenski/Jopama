1cb5d9404fc30b13df950517b7a2cd8b6cad0d00
Maciej Smolenski <jezdnia@gmail.com>
2017-12-11 23:38:19 +0100

    Minor changes in test scripts.

--- docker/runIt.sh
+++ docker/runIt.sh
@@ -9,7 +9,10 @@ function runOnce()
     local id=$1
     echo "Running: $id"
     sudo rm -fr /var/jopamaTest/storage/ /var/jopamaTest/logs
-    python testRunner.py -ips 127.0.0.1 -numClusters 1 -clusterSize 3 -numTP 1 -numTC 1 -firstComp 100 -numComp 1000 -compsInTra 10 -outForTC 10 -outForTP 5 -duration 300
+    python testRunner.py -ips 127.0.0.1 -numClusters 1 -clusterSize 1 -numTP 1 -numTC 1 -firstComp 100 -numComp 100 -compsInTra 2 -outForTC 10 -outForTP 2 -duration 30
+    local retVal=$?
+    echo "python finished with $retVal"
+    return $retVal
 }
 
 function runNumTimes()
@@ -19,10 +22,12 @@ function runNumTimes()
         return 1
     fi
     local numRuns=$1
+    local retVal=0
     for ((i=0;i<${numRuns};i++)); do
-        runOnce $i
-        if [[ $? -ne 0 ]]; then
-            echo "python FAILURE"
+        { runOnce $i 2>&1; } | tee ~/dockerLogs/testRunner_$(date +%s).log
+        retVal=$?
+        if [[ $retVal -ne 0 ]]; then
+            echo "python FAILURE $retVal"
             return 1
         fi
     done
