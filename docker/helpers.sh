4275997d05063279fd5e3b5d5f3e9cbb70d13308
Maciej Smolenski <jezdnia@gmail.com>
2018-05-06 00:28:43 +0200

    createMachines, destroyMachines, setUpMachinesPull - gnu parallel used to speed testing up. Convenient function performTestForMult for scalability/perf testing added.

--- docker/helpers.sh
+++ docker/helpers.sh
@@ -10,12 +10,41 @@ function runOnce()
     #python testRunner.py -dockerRunnerArg NATIVE:3 -numClusters 2 -clusterSize 3 -numTP 1 -numTC 1 -firstComp 100 -numComp 10000 -compsInTra 10 -outForTC 100 -outForTP 20 -duration 180
     #python testRunner.py -dockerRunnerArg NATIVE:1 -outputDir /tmp/jopamaResults -numClusters 1 -clusterSize 1 -numTP 1 -numTC 1 -firstComp 100 -numComp 10000 -compsInTra 10 -outForTC 400 -outForTP 200 -duration 180
     #python testRunner.py -dockerRunnerArg NATIVE:1 -outputDir /tmp/jopamaResults -numClusters 3 -clusterSize 1 -numTP 1 -numTC 1 -firstComp 100 -numComp 100000 -compsInTra 2 -outForTC 100 -outForTP 10 -duration 180
-    python testRunner.py -dockerRunnerArg "DOCKERMACHINE:ens5;myengine000" -outputDir /tmp/jopamaResults -numClusters 1 -clusterSize 3 -numTP 3 -numTC 1 -firstComp 100 -numComp 100000 -compsInTra 2 -outForTC 100 -outForTP 10 -duration 180
+    python testRunner.py -dockerRunnerArg "DOCKERMACHINE:ens5;myengine000,myengine001,myengine002" -outputDir /tmp/jopamaResults -numClusters 4 -clusterSize 3 -numTP 3 -numTC 1 -firstComp 100 -numComp 100000 -compsInTra 2 -outForTC 100 -outForTP 10 -duration 180
     #python testRunner.py -dockerRunnerArg "DOCKERMACHINE:eth0;myengine000" -outputDir /tmp/jopamaResults -numClusters 1 -clusterSize 1 -numTP 1 -numTC 1 -firstComp 100 -numComp 10000 -compsInTra 10 -outForTC 100 -outForTP 20 -duration 180
     #python testRunner.py -dockerRunnerArg "DOCKERMACHINE:ens5;myengine000" -outputDir /tmp/jopamaResults -numClusters 1 -clusterSize 3 -numTP 1 -numTC 1 -firstComp 100 -numComp 100000 -compsInTra 10 -outForTC 100 -outForTP 10 -duration 180
     local retVal=$?
     echo "python finished with $retVal"
     return $retVal
+}
+
+function getMachinesString()
+{
+    if [[ $# -ne 1 ]]; then
+        echo "getMachinesString args"
+        return 1
+    fi
+    local mult=$1
+    local str=""
+    for ((n=0;n<3*mult;++n)); do
+        if [[ $n -gt 0 ]]; then
+            str="${str},"
+        fi
+        str="${str}$(getMachineName $n)"
+    done
+    echo $str
+}
+
+function performTestForMult()
+{
+    if [[ $# -ne 1 ]]; then
+        echo "performTestForMult args"
+        return 1
+    fi
+    local mult=$1
+    local mstr="$(getMachinesString $mult)"
+    local numClusters=$((4 * mult))
+    { python testRunner.py -dockerRunnerArg "DOCKERMACHINE:ens5;${mstr}" -outputDir /tmp/jopamaResults -numClusters $numClusters -clusterSize 3 -numTP 3 -numTC 1 -firstComp 100 -numComp 100000 -compsInTra 2 -outForTC 100 -outForTP 10 -duration 180 2>&1; } | tee ~/dockerLogs/testRunner_$(date +%Y%m%d_%H%M%S).log
 }
 
 function runNumTimes()
@@ -36,12 +65,22 @@ function runNumTimes()
     done
 }
 
-NUM_MACHINES=1
-JOPAMA_DIR=/var/jopamaTest
-DM_DRIVER=kvm
-DM_DRIVER=amazonec2
+NUM_MACHINES=3
+export JOPAMA_DIR=/var/jopamaTest
+export DM_DRIVER=kvm
+export DM_DRIVER=amazonec2
 
 source ~/docker-machine-aws
+
+function myParallel()
+{
+    parallel --jobs 0 $*
+}
+
+function getMachineIDs()
+{
+    seq 0 $((NUM_MACHINES - 1))
+}
 
 function getMachineName()
 {
@@ -52,7 +91,16 @@ function getMachineName()
     printf "myengine%03d" $1
 }
 
-function setUpMachine()
+export -f getMachineName
+
+function myDockerMachine()
+{
+    /home/barbara/docker/docker-machine $*
+}
+
+export -f myDockerMachine
+
+function _setUpMachine()
 {
     if [[ $# -ne 1 ]]; then
         echo "arguments"
@@ -60,9 +108,24 @@ function setUpMachine()
     fi
     local id=$1
     local name=$(getMachineName $id)
-    docker-machine ssh $name sudo mkdir ${JOPAMA_DIR}
-    docker-machine ssh $name sudo mount -t ramfs -o size=1536M ramfs ${JOPAMA_DIR}
-    docker-machine ssh $name sudo chmod 777 ${JOPAMA_DIR}
+    myDockerMachine ssh $name sudo mkdir ${JOPAMA_DIR}
+    myDockerMachine ssh $name sudo mount -t ramfs -o size=2048M ramfs ${JOPAMA_DIR}
+    myDockerMachine ssh $name sudo chmod 777 ${JOPAMA_DIR}
+}
+
+export -f _setUpMachine
+
+function setUpMachineSaveLoad()
+{
+    local id=$1
+    local name=$(getMachineName $id)
+    local archives=( $(echo $2) )
+    _setUpMachine $id
+    eval $(myDockerMachine env $name)
+    for ((j=0;j<${#archives[*]};j++)); do
+        docker load -i ${archives[$j]}
+    done
+    eval $(myDockerMachine env -u)
 }
 
 function setUpMachinesSaveLoad()
@@ -74,42 +137,56 @@ function setUpMachinesSaveLoad()
         docker save -o ${archives[$i]} ${imgs[$i]}
     done
     for ((i=0;i<${NUM_MACHINES};++i)); do
-        setUpMachine $i
-        local name=$(getMachineName $i)
-        eval $(docker-machine env $name)
-        for ((j=0;j<$numImages;j++)); do
-            docker load -i ${archives[$j]}
-        done
-        eval $(docker-machine env -u)
+        setUpMachineSaveLoad $i "${archives[*]}"
     done
 }
+
+function _setUpMachinePull()
+{
+    local id=$1
+    local name=$(getMachineName $id)
+    local imgs=(smolenski/zookeeper smolenski/jopama)
+    local numImages=${#imgs[*]}
+    _setUpMachine $id
+    eval $(myDockerMachine env $name)
+    for ((j=0;j<$numImages;j++)); do
+        docker pull ${imgs[$j]}
+    done
+    eval $(myDockerMachine env -u)
+}
+
+export -f _setUpMachinePull
 
 function setUpMachinesPull()
 {
-    local imgs=(smolenski/zookeeper smolenski/jopama)
-    local numImages=${#imgs[*]}
-    for ((i=0;i<${NUM_MACHINES};++i)); do
-        setUpMachine $i
-        local name=$(getMachineName $i)
-        eval $(docker-machine env $name)
-        for ((j=0;j<$numImages;j++)); do
-            docker pull ${imgs[$j]}
-        done
-        eval $(docker-machine env -u)
-    done
+    getMachineIDs | myParallel _setUpMachinePull {1}
 }
+
+function _createMachine()
+{
+    local id=$1
+    local name=$(getMachineName $id)
+    myDockerMachine create --driver $DM_DRIVER $name
+}
+
+export -f _createMachine
 
 function createMachines()
 {
-    for ((i=0;i<${NUM_MACHINES};++i)); do
-        docker-machine create --driver $DM_DRIVER $(getMachineName $i)
-    done
+    getMachineIDs | myParallel _createMachine {1}
 }
+
+function _destroyMachine()
+{
+    local id=$1
+    local name=$(getMachineName $id)
+    myDockerMachine stop $name
+    myDockerMachine rm -f $name
+}
+
+export -f _destroyMachine
 
 function destroyMachines()
 {
-    for ((i=0;i<${NUM_MACHINES};++i)); do
-        docker-machine stop $(getMachineName $i)
-        docker-machine rm -f $(getMachineName $i)
-    done
+    getMachineIDs | myParallel _destroyMachine {1}
 }
